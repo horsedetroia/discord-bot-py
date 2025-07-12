47c95c1db48b641802920c7290de51a2f16b0619
Sarah <149827383+horsedetroia@users.noreply.github.com>
2025-07-11 21:55:07 -0500

    Create code01.py

---
+++ discord-py/code01.py
@@ -0,0 +1,9 @@
+### COMANDO COM APENAS FUNÇÕES NECESSÁRIAS PARA O BOT FUNCIONAR!
+
+import discord # Importa a biblioteca que nos permite usar recursos do Discord, como criar eventos, e usar discord.Client.
+
+intents = discord.Intents.default() # Declara os recursos que o bot pode acessar (como membros de um servidor, id de canais...). É como dizer 'os recursos (intents) dos recursos do Discord (discord.Intents) que o bot pode acessar, é igual a x (default())'.
+
+client = discord.Client(intents=intents) #Isso cria nosso bot. É como dizer 'o bot (client), cadastrado como cliente do Discord (discord.Client), pode acessar os recursos definidos acima (intents=intents)'
+
+client.run('Seu token') # Coloca o bot para iniciar
