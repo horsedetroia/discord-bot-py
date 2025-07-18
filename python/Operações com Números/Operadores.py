d4f7fbe72f1589f135335fea0fd598371d5ddc1e
Sarah <149827383+horsedetroia@users.noreply.github.com>
2025-07-17 22:35:56 -0500

    Update Operadores.py

--- python/Operações com Números/Operadores.py
+++ python/Operações com Números/Operadores.py
@@ -1,8 +1,27 @@
-No Python, as formas de simbolizar uma operação são  diferentes das tradicionalmente usadas em matemática. Portanto, os operadores aritméticos são:
+# É possível realizar cálculos, desde os mais simples aos mais complexos utilizando uma linguagem de programação. No Python, as formas de simbolizar uma operação são  diferentes das tradicionalmente usadas em matemática. Portanto, os operadores aritméticos são:
 
+"""
 '+' - para indicar adições (ex.: 1+1, com resultado 2)
 '==' - para indicar igualdade (ex.: 2==1+1, com resultado verdadeiro)
 '/' - para divisões (ex.: 6/3, com resultado 2, ou 7/3, com resultado 2.3333...)
 '//' - para divisões exatas (ex.: 6//3, com resultado 2, ou 7//3, com resultado 2)
 '*' - para multiplicações (ex.: 5*3, com resultado 15)
 '**' - para potências (ex.: 3**2, com resultado 9)
+"""
+
+# Exemplos a seguir:
+print('5 - 5 ==', 5 - 5) # Exibe uma subração na tela
+
+print('5 + 5 ==', 5 + 5) # Exibe uma adição na tela
+
+print('5 é igual a 7?', 5 == 7) # Informa se a comparação 5 == 7 é verdadeira ou falsa
+
+print('5 == 5?', 5 == 5) #  Informa se 5 é igual a 5
+
+print('Quanto é 15 dividido por 5?', 15/5) # Informa o valor de uma divisão
+
+print('Quanto é 15 dividido por 5?', 15//5) # Informa o resultado de uma divisão inteira
+
+print('Quanto é 7 dividido por 3', 7/3) # Informa o valor da divisão
+
+print('Quanto é 7 dividido por 3?', 7//3) # Informa o valor de uma divisão inteira
